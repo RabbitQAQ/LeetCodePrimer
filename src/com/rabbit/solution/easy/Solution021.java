5ac1f8b5a3c6dd7bcf696c262ce6d969726f4abe
RabbitQAQ <charles_dai@qq.com>
2019-06-19 23:11:38 -0500

    solution 021 update

--- src/com/rabbit/solution/easy/Solution021.java
+++ src/com/rabbit/solution/easy/Solution021.java
@@ -6,65 +6,27 @@ import com.rabbit.solution.utils.ListNode;
 import java.util.List;
 
 public class Solution021 {
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        ListNode root = l1;
-        ListNode prev = null;
-        ListNode l2Temp = l2;
-        do {
-            if (l2 == null) {
-                break;
-            }
-            if (l1.val > l2.val) {
-                if (prev == null) {
-                    l2Temp = l2.next;
-                    l2.next = l1;
-                    prev = l2;
-                    root = l2;
-                    l2 = l2Temp;
-                } else {
-                    l2Temp = l2.next;
-                    prev.next = l2;
-                    l2.next = l1;
-                    prev = l2;
-                    l2 = l2Temp;
-                }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode curr = new ListNode(-1);
+        ListNode dummyHead = curr;
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+        while (t1 != null || t2 != null) {
+            if ((t1 == null ? true : (t2 == null ? false : t1.val > t2.val))) {
+                curr.next = t2;
+                t2 = t2.next;
+                curr = curr.next;
             } else {
-                prev = l1;
-                l1 = l1.next;
+                curr.next = t1;
+                t1 = t1.next;
+                curr = curr.next;
             }
         }
-        while (l1 != null);
 
-        if (l2 != null) {
-            prev.next = l2;
-        }
-
-        return root;
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
-        ListNode ln1 = new ListNode(1);
-        ListNode ln2 = new ListNode(2);
-        ListNode ln3 = new ListNode(4);
-        ListNode ln4 = new ListNode(5);
-        ListNode ln5 = new ListNode(3);
-        ListNode ln6 = new ListNode(4);
 
-        ln1.next = ln2;
-        ln2.next = ln3;
-//        ln4.next = ln5;
-//        ln5.next = ln6;
-
-        ListNode root = mergeTwoLists(ln4, ln1);
-        while (root != null) {
-            System.out.print(root);
-            root = root.next;
-        }
     }
 }
