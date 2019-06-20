a73004f2884cba501ca1c31adefdc91f281215da
RabbitQAQ <charles_dai@qq.com>
2019-06-20 00:19:11 -0500

    solution 053 update

--- src/com/rabbit/solution/easy/Solution053.java
+++ src/com/rabbit/solution/easy/Solution053.java
@@ -1,29 +1,56 @@
 package com.rabbit.solution.easy;
 
 public class Solution053 {
-    public static int maxSubArray(int[] nums) {
+    // Divide and Conquer
+    public int crossSum(int[] nums, int left, int right, int mid) {
+        if (left >= right) {
+            return nums[left];
+        }
+        int currLeft = 0;
+        int maxLeft = Integer.MIN_VALUE;
+        for (int i = mid; i > left; i--) {
+            currLeft += nums[i];
+            maxLeft = Math.max(currLeft, maxLeft);
+        }
+
+        int currRight = 0;
+        int maxRight = Integer.MIN_VALUE;
+        for (int i = mid + 1; i <= right; i++) {
+            currRight += nums[i];
+            maxRight = Math.max(currRight, maxRight);
+        }
+
+        return maxLeft + maxRight;
+    }
+
+    public int helper(int[] nums, int left, int right) {
+        if (left >= right) {
+            return nums[left];
+        }
+        int mid = (left + right) / 2;
+        int maxLeft = helper(nums, left, mid - 1);
+        int maxRight = helper(nums, right, mid + 1);
+        int maxCross = crossSum(nums, left, right, mid);
+
+        return Math.max(maxCross, Math.max(maxLeft, maxRight));
+    }
+
+    public int maxSubArray(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    // Greedy
+    public int maxSubArray2(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-
-        int[] currMax = new int[nums.length];
-        currMax[0] = nums[0];
+        int res = nums[0];
+        int currSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            currMax[i] = Integer.max(nums[i], currMax[i - 1] + nums[i]);
+            currSum = Math.max(nums[i], nums[i] + currSum);
+            res = Math.max(res, currSum);
         }
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < currMax.length; i++) {
-            if (currMax[i] > max) {
-                max = currMax[i];
-            }
-        }
-
-        return max;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(nums));
+        return res;
     }
 }
