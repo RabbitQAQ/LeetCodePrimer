8609f83344a28b6c0e0db1d3cdd0618666cb4306
RabbitQAQ <charles_dai@qq.com>
2019-06-21 16:59:51 -0500

    solution 053 update

--- src/com/rabbit/solution/easy/Solution206.java
+++ src/com/rabbit/solution/easy/Solution206.java
@@ -6,71 +6,56 @@ import com.rabbit.solution.utils.Utils;
 public class Solution206 {
     // Reverse Linked List
     // 遍历的方式
-    public static ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
             return head;
         }
+        ListNode res = head;
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
-            ListNode temp = curr.next;
+            ListNode tmp = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = temp;
+            curr = tmp;
         }
 
         return prev;
     }
 
     // 递归的方式
-    public static ListNode reverseListRecursively(ListNode head) {
+    public ListNode reverseList2(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode result = head;
-        while (result.next != null) {
-            result = result.next;
-        }
-        recursively(head);
-        return result;
+        ListNode tmp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tmp;
     }
 
-    static ListNode recursively(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        ListNode temp = recursively(head.next);
-        if (temp != null) {
-            temp.next = head;
-            head.next = null;
-        }
-        return head;
-    }
-
-    // 更好的递归做法  (C style)
-//    class Solution {
-//        public:
-//        ListNode* reverseList(ListNode* head) {
-//            if (!head || !head->next) return head;
-//            ListNode *p = head;
-//            head = reverseList(p->next);
-//            p->next->next = p;
-//            p->next = NULL;
+    // Archived Code
+//    public static ListNode reverseListRecursively(ListNode head) {
+//        if (head == null || head.next == null) {
 //            return head;
 //        }
-//    };
-
-    public static void main(String[] args) {
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        ListNode listNode3 = new ListNode(3);
-        ListNode listNode4 = new ListNode(4);
-        ListNode listNode5 = new ListNode(5);
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-        listNode3.next = listNode4;
-        listNode4.next = listNode5;
-
-        Utils.printListNode(reverseListRecursively(listNode1));
-    }
+//        ListNode result = head;
+//        while (result.next != null) {
+//            result = result.next;
+//        }
+//        recursively(head);
+//        return result;
+//    }
+//
+//    static ListNode recursively(ListNode head) {
+//        if (head == null) {
+//            return null;
+//        }
+//        ListNode temp = recursively(head.next);
+//        if (temp != null) {
+//            temp.next = head;
+//            head.next = null;
+//        }
+//        return head;
+//    }
 }
