0f5e714cb9b89d07b858fbd2b433565db9ef63a8
RabbitQAQ <charles_dai@qq.com>
2019-06-24 20:11:05 -0500

    solution 004 update

--- src/com/rabbit/solution/hard/Solution004.java
+++ src/com/rabbit/solution/hard/Solution004.java
@@ -3,36 +3,27 @@ package com.rabbit.solution.hard;
 import java.util.Arrays;
 
 public class Solution004 {
-    // 第一次遇到hard难度的题。。真不愧是hard难度。。。
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
-        int left = (m + n + 1) / 2, right = (m + n + 2) / 2;
-        return (findKth(nums1, nums2, left) + findKth(nums1, nums2, right)) / 2.0;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int mid1 = (nums1.length + nums2.length + 1) / 2;
+        int mid2 = (nums1.length + nums2.length + 2) / 2;
+        return (findKthNumber(nums1, 0, nums2, 0, mid1) + findKthNumber(nums1, 0, nums2, 0, mid2)) / 2.0;
     }
 
-    public static int findKth(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length;
-        int n = nums2.length;
-        if (m > n) {
-            return findKth(nums2, nums1, k);
-        }
-        if (m == 0) {
-            return nums2[k - 1];
-        }
-        if (k == 1) {
-            return nums1[0] > nums2[0] ? nums2[0] : nums1[0];
-        }
-        int i = Math.min(m, k / 2), j = Math.min(n, k / 2);
-        if (nums1[i - 1] > nums2[j - 1]) {
-            return findKth(nums1, Arrays.copyOfRange(nums2, j, n), k - j);
+    public double findKthNumber(int[] nums1, int i, int[] nums2, int j, int K) {
+        if (i >= nums1.length) {
+            return nums2[j + K - 1];
+        } else if (j >= nums2.length) {
+            return nums1[i + K - 1];
+        } else if (K == 1) {
+            return Math.min(nums1[i], nums2[j]);
         } else {
-            return findKth(nums2, Arrays.copyOfRange(nums1, i, m), k - i);
+            int mid1 = (i + (K / 2) - 1 < nums1.length ? nums1[i + (K / 2) - 1] : Integer.MAX_VALUE);
+            int mid2 = (j + (K / 2) - 1 < nums2.length ? nums2[j + (K / 2) - 1] : Integer.MAX_VALUE);
+            if (mid1 < mid2) {
+                return findKthNumber(nums1, i + K / 2, nums2, j, K - K / 2);
+            } else {
+                return findKthNumber(nums1, i, nums2, j + K / 2, K - K / 2);
+            }
         }
-    }
-
-    public static void main(String[] args) {
-        int[] test1 = {1};
-        int[] test2 = {3,4};
-        System.out.println(findMedianSortedArrays(test1, test2));
     }
 }
