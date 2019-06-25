00a008fdbe51c284205ceb388138ec4a2c85e870
RabbitQAQ <charles_dai@qq.com>
2019-06-24 22:39:51 -0500

    solution 973 update

---
+++ src/com/rabbit/solution/medium/Solution973.java
@@ -0,0 +1,50 @@
+package com.rabbit.solution.medium;
+
+import java.util.Arrays;
+
+public class Solution973 {
+    int[][] points;
+
+    public int[][] kClosest(int[][] points, int K) {
+        this.points = points;
+        quickSort(0, points.length - 1);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    public void quickSort(int left, int right) {
+        if (points.length < 2 || left >= right) {
+            return;
+        }
+        int l = left + 1, r = right;
+        int partition = calDistance(left);
+        while (true) {
+            while (l < r && calDistance(l) <= partition) {
+                l++;
+            }
+            while (l <= r && calDistance(r) >= partition) {
+                r--;
+            }
+            if (l >= r) {
+                break;
+            }
+            swap(l, r);
+        }
+        swap(left, r);
+        partition = r;
+        quickSort(left, partition - 1);
+        quickSort(partition + 1, right);
+    }
+
+    public int calDistance(int i) {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
+    }
+
+    public void swap(int i, int j) {
+        int tmpx = points[i][0];
+        int tmpy = points[i][1];
+        points[i][0] = points[j][0];
+        points[i][1] = points[j][1];
+        points[j][0] = tmpx;
+        points[j][1] = tmpy;
+    }
+}
--- src/com/rabbit/solution/others/Solution002.java
+++ src/com/rabbit/solution/others/Solution002.java
@@ -2,31 +2,61 @@ package com.rabbit.solution.others;
 
 public class Solution002 {
     // 快速排序
+//    public static void quickSort(int[] input, int start, int end) {
+//        int l = start, r = end;
+//        int key = input[l];
+//        while (l < r) {
+//            while (r > l) {
+//                if (input[r] < key) {
+//                    int temp = input[r];
+//                    input[r] = input[l];
+//                    input[l] = temp;
+//                    break;
+//                }
+//                r--;
+//            }
+//            while (l < r) {
+//                if (input[l] > key) {
+//                    int temp = input[r];
+//                    input[r] = input[l];
+//                    input[l] = temp;
+//                    break;
+//                }
+//                l++;
+//            }
+//        }
+//        if (r < end) quickSort(input, r + 1, end);
+//        if (l > start) quickSort(input, start, l - 1);
+//    }
+
     public static void quickSort(int[] input, int start, int end) {
+        if (start >= end)
+            return;
         int l = start, r = end;
-        int key = input[l];
-        while (l < r) {
-            while (r > l) {
-                if (input[r] < key) {
-                    int temp = input[r];
-                    input[r] = input[l];
-                    input[l] = temp;
-                    break;
-                }
-                r--;
-            }
-            while (l < r) {
-                if (input[l] > key) {
-                    int temp = input[r];
-                    input[r] = input[l];
-                    input[l] = temp;
-                    break;
-                }
+        int partition = input[l];
+        while(true) {
+            while(l <= r && input[l] <= partition)
                 l++;
-            }
+            while(l <= r && input[r] >= partition)
+                r--;
+            if (l >= r)
+                break;
+            int tmp = input[l];
+            input[l] = input[r];
+            input[r] = tmp;
         }
-        if (r < end) quickSort(input, r + 1, end);
-        if (l > start) quickSort(input, start, l - 1);
+
+        int tmp = input[start];
+        input[start] = input[r];
+        input[r] = tmp;
+        quickSort(input, start, r - 1);
+        quickSort(input, r + 1, end);
+    }
+
+    public static void swap(int[] input, int i, int j) {
+        int tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
     }
 
     public static void main(String[] args) {
